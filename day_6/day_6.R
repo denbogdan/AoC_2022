6bb34ad2881d9e82b085bb873eae06875a1e5318
Denisa Bogdan <dbogdan@DMBP14.local>
2022-12-07 00:12:30 +0000

    day 6

--- day_6/day_6.R
+++ day_6/day_6.R
@@ -2,31 +2,25 @@ data <- readLines("~/projects/AoC_2022/day_6/input_day6.txt")
 data <- strsplit(data[1], split="") |> unlist()
 
 ##Question 1 -----------------------------------------------------------------------
-found <- FALSE
-i <- 1
-while(!found) {
-  marker <- data[i:(i+3)]
-  if(length(unique(marker)) == 4)
-    found=TRUE
-  else
-    i=i+1
+find_marker <- function(data, nchar) {
+  found <- FALSE
+  i <- 1
+  while(!found) {
+    marker <- data[i:(i+(nchar-1))]
+    if(length(unique(marker)) == nchar){
+      found=TRUE
+      return(i+(nchar-1))
+    }
+    else
+      i=i+1
+  }
 }
 
-##Answer 1 -------------------------------------------------------------------------
-i+3
-
-##Question 2 -----------------------------------------------------------------------
-found <- FALSE
-i <- 1
-while(!found) {
-  marker <- data[i:(i+13)]
-  if(length(unique(marker)) == 14)
-    found=TRUE
-  else
-    i=i+1
-}
 
 ##Answer 1 -------------------------------------------------------------------------
-i+13
+find_marker(data, 4)
+
+##Answer 2 -------------------------------------------------------------------------
+find_marker(data, 14)
 
 
